9055633f717ee79087f784e0e58ec3f2d197a02e
Mohammad Erfan Najafi <ebfin.work@gmail.com>
2025-08-24 20:35:04 +0200

    Missions
    new missions system

--- Assets/Scripts/Erfan/Scriptable Objects/Mission2.cs
+++ Assets/Scripts/Erfan/Scriptable Objects/Mission2.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Mission2", menuName = "Scriptable Objects/Mission2")]
 public class Mission2 : ScriptableObject
 {
-    
+    public string nameMission;
+    public string description;
+    public string codeNeedCheck;
+    public bool isDone;
 }
